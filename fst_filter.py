d30fbbe9c165db2c520f81f65904717423dee8c4
Tyler K. Chafin <tkchafin@uark.edu>
2017-09-19 17:27:41 -0500

    tested basic functions

--- fst_filter.py
+++ fst_filter.py
@@ -4,4 +4,34 @@ import os
 import sys
 import Bio
 from Bio import AlignIO
-import aln_file_tools.py as
+import aln_file_tools as aft
+import alignment_tools as a
+from fst_filter_menu import parseArgs
+
+"""IMPORTANT NOTE:
+Pausing development for now because I found an easier way to get what I want.
+May resume working on this later. """
+
+
+
+#Need:
+	#Vector of population sizes
+	#Mean population size of each
+
+
+
+#For each Locus:
+	#For each allele:
+		#Calculate heterozygosity per pop
+		#Calculate frequency per pop
+		#Calculate average het of allele
+		#Calculate average freq of allele
+		#Calculate sample variance of allele
+		#Calculate average pop size and variance
+
+params = parseArgs()
+
+aln = aft.read_phylip(params.input)
+
+for col in a.aln_column_iterator(aln):
+	print(col)
